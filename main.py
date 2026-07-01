af2b68a390d16239389eb8a791a03b8e0854d138
santiagorwd22 <rodriguezdelgadilloes06@gmail.com>
2026-07-01 10:25:29 -0500

    primer intento laboratorio github

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 for i in range(1, 1001):
-    if i % 5 == 0:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
         print("Buzz")
     elif i % 3 == 0:
         print("Fizz")
